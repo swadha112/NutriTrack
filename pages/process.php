3088638ea9d277ffd130b34c66c0cf68e0937ec4
swadha112 <swadha.khatod07@gmail.com>
2024-03-27 00:35:02 +0530

    php validation

--- pages/process.php
+++ pages/process.php
@@ -1,6 +1,5 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
     $username = $_POST['username'];
     $firstname = $_POST['firstName'];
     $lastname = $_POST['lastName'];
@@ -8,25 +7,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $height = $_POST['height'];
     $weight = $_POST['weight'];
     $gender = $_POST['gender'];
-   
-    // Validate and sanitize input if necessary
+    $errors = [];
 
-    // Insert additional information into the database
-    // Replace 'your_host', 'your_database_name', 'your_username', and 'your_password' with your actual database credentials
-    $db = new PDO("pgsql:host=localhost;dbname=nutritrack", 'postgres', 'swadhak');
-    $sql = "UPDATE users SET  age = :age, height = :height, weight = :weight, firstname = :firstname, lastname = :lastname, gender = :gender WHERE username = :username";
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':username', $username);
-    $stmt->bindParam(':firstname', $firstname);
-    $stmt->bindParam(':lastname', $lastname);
-    $stmt->bindParam(':age', $age);
-    $stmt->bindParam(':height', $height);
-    $stmt->bindParam(':weight', $weight);
-    $stmt->bindParam(':gender', $gender);
-    $stmt->execute();
-   
-    // Redirect to dashboard or another appropriate page after successful submission
-    header("Location: dashboard.html");
-    exit();
+    if ($height < 0 || $height > 300) {
+        $errors[] = "Height must be between 0 and 300 cm.";
+    }
+
+    if ($weight < 0) {
+        $errors[] = "Weight must be a positive value.";
+    }
+
+    if (!empty($errors)) {
+
+        foreach ($errors as $error) {
+            echo "<p>Error: $error</p>";
+        }
+    } else {
+        
+        $db = new PDO("pgsql:host=localhost; dbname=nutritrack", 'postgres', 'swadhak');
+        $sql = "UPDATE users SET age = :age, height = :height, weight = :weight, firstname = :firstname, lastname = :lastname, gender = :gender WHERE username = :username";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':firstname', $firstname);
+        $stmt->bindParam(':lastname', $lastname);
+        $stmt->bindParam(':age', $age);
+        $stmt->bindParam(':height', $height);
+        $stmt->bindParam(':weight', $weight);
+        $stmt->bindParam(':gender', $gender);
+        $stmt->execute();
+       
+        
+        header("Location: dashboard.html");
+        exit();
+    }
 }
 ?>
